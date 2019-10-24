e7fb31ce8b9a366303bb41bd04319e0d1d8483d3
chaptone <rakkan.j@gmail.com>
2019-10-24 18:32:12 +0700

    Update api router

--- router/api.js
+++ router/api.js
@@ -11,6 +11,8 @@ router.get('/', async (req, res) => {
 router.get('/forex', async (req, res) => {
   console.log(process.env.FLASK_URL)
   console.log(process.env.FLASK_PORT)
+  console.log(config.flask_url)
+  console.log(config.flask_port)
   const response = await axios({
     method: 'get',
     url: `http://${config.flask_url}:${config.flask_port}`,
